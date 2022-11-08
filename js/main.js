5cdcda162b690bea712b6c5f18b046161e0acee6
Pedra96 <darkraidialga@yahoo.it>
2022-11-08 16:36:48 +0100

    completato esercizio

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 let nome = prompt("qual'è il tuo nome");
 let cognome = prompt("qual'è il tuo cognome?");
 let colore = prompt("qual'è il tuo colore preferito?");
-let numero = "21";
+let numero1 = prompt("dammi un numero");
+let numero2 =prompt("dammi un altro numero");
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
 
-let password= nome+cognome+colore+numero;
+let summ=numero1/numero2;
+summ=Math.round(summ);
+let password= nome+cognome+colore+summ;
 
 document.getElementById("password").innerHTML = password;
 document.getElementById("nome").innerHTML = nome;
+document.getElementById("cognome").innerHTML = cognome;
+document.getElementById("colore").innerHTML = colore;
+document.getElementById("numero").innerHTML = summ;
