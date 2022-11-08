d426a9f81fdd7970d230f7a0423d9703a71836b0
Pedra96 <darkraidialga@yahoo.it>
2022-11-08 17:44:57 +0100

    aggiunto check per le let int se sono NaN

--- js/main.js
+++ js/main.js
@@ -2,9 +2,23 @@ let nome = prompt("qual'è il tuo nome");
 let cognome = prompt("qual'è il tuo cognome?");
 let colore = prompt("qual'è il tuo colore preferito?");
 let numero1 = prompt("dammi un numero");
-let numero2 =prompt("dammi un altro numero");
 numero1 = parseInt(numero1);
+if(isNaN(numero1) == true){
+   do{
+    numero1=prompt("inserisci un numero non lettere");
+    numero1 = parseInt(numero1);
+   } while(isNaN(numero1) == true);
+}
+
+
+let numero2 =prompt("dammi un altro numero");
 numero2 = parseInt(numero2);
+if(isNaN(numero2) == true){
+   do{
+    numero2=prompt("inserisci un numero non lettere");
+    numero2 = parseInt(numero2);
+   } while(isNaN(numero2) == true);
+}
 
 let summ=numero1/numero2;
 summ=Math.round(summ);
